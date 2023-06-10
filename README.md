961628029370e1fd0ba6bc043d3017e0c47edffb
Khoa Hoang <koadahoakodah@gmail.com>
2023-06-09 23:39:50 -0400

    set up CMake project

---
+++ README.md
@@ -0,0 +1,13 @@
+# renderer
+
+## getting started
+
+using the script
+- `chmod +x run` to add executable permissions to the run script file
+- `./run`
+
+manually:
+- `cd` into the root directory of this project
+- `cmake -S . -B build` to initialize cmake
+- `cmake --build build` to build project
+- `./build/bin/renderer` to run project
---
+++ src/main.cpp
@@ -0,0 +1,67 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+// handle resizing of GLFW window
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+// handle keyboard input
+void process_input(GLFWwindow *window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    // initialize GLFW
+    if (!glfwInit()) {
+        std::cerr << "failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+    
+    // configure GLFW window properties
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // create GLFW window
+    GLFWwindow *window = glfwCreateWindow(800, 600, "OpenGL Test", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    
+    // initialize GLEW
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "failed to initialize GLEW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    // set viewport dimensions
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    
+    // register framebuffer size callback function
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // main render loop
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    return 0;
+}
